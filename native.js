0a9509a4bd60cf12db1cf58f26a01658634599a5
Marcus Christiansson <christiansson.marcus@gmail.com>
2022-09-30 18:22:20 +0200

    Added rules for hooks.

---
+++ native.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: "./index.js",
+  plugins: ["react-native"],
+  env: {
+    "react-native/react-native": true,
+  },
+};
---
+++ web.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: "./index.js",
+  env: {
+    browser: true,
+  },
+};
